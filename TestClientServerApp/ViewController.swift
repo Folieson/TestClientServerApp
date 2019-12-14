b030df8cc1f7d914e8aede0bce95c36d58bfae59
Andrey Ponamarchuk <ponamarchuk.andrey@icloud.com>
2019-12-14 12:46:58 +0500

    Initial Commit

---
+++ TestClientServerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestClientServerApp
+//
+//  Created by Андрей Понамарчук on 14.12.2019.
+//  Copyright © 2019 Андрей Понамарчук. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
