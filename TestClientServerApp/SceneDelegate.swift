605a45e1114685407b12dec1a96536461b4f3a5e
Andrey Ponamarchuk <ponamarchuk.andrey@icloud.com>
2019-12-15 12:41:33 +0500

    main.storyboard removed

--- TestClientServerApp/SceneDelegate.swift
+++ TestClientServerApp/SceneDelegate.swift
@@ -16,8 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        window?.windowScene = windowScene
+        window = UIWindow(windowScene: windowScene)
         let mainVC = ModuleBuilder.createMain()
         let navBar = UINavigationController(rootViewController: mainVC)
         window?.rootViewController = navBar
